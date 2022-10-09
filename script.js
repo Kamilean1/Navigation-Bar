1c0ea36d528050f3f2b9323f44de7b3bc657cf25
Kamilean1 <misskamile.a@gmail.com>
2022-10-09 02:02:43 +0200

    corrected the hamburger

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-const menu = document.querySelector(".Nav-bar");
-const menuItems = document.querySelectorAll(".Button");
-const hamburger = document.querySelector(".")
+const hamburger = document.querySelector('.toggle-button');
+const hamburger_icon = hamburger.querySelector('span');
+const mobile_menu = document.querySelector('.Navbar');
+
+hamburger.addEventListener('click', () => {
+    hamburger_icon.innerText = hamburger_icon.innerText === 'Menu'
+     ? 'close'
+     : 'Menu';
+
+     mobile_menu.classList.toggle('is-open');
+})
